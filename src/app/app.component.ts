9b1822378ac880f1032e9a604d15ad5496be0384
Mokuichi <38586357+Mokuichi147@users.noreply.github.com>
2020-05-30 18:25:46 +0900

    [update] 余計なコードの削除

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,6 @@ export class AppComponent implements OnInit {
   title = 'Mokuichi147-Pages';
 
   // screen size
-  screen_size = 'm';
   small_menu = false;
 
 
@@ -28,22 +27,7 @@ export class AppComponent implements OnInit {
       Breakpoints.Large,
       Breakpoints.XLarge
     ]).subscribe( (state: BreakpointState) => {
-      if (state.breakpoints[Breakpoints.XSmall]) {
-        this.screen_size = 'xs';
-      } else if (state.breakpoints[Breakpoints.Small]) {
-        this.screen_size = 's';
-      } else if (state.breakpoints[Breakpoints.Medium]) {
-        this.screen_size = 'm';
-      } else if (state.breakpoints[Breakpoints.Large]) {
-        this.screen_size = 'l';
-      } else if (state.breakpoints[Breakpoints.XLarge]) {
-        this.screen_size = 'xl';
-      }
-      if (this.screen_size == 'xs') {
-        this.small_menu = true;
-      } else {
-        this.small_menu = false;
-      }
+      this.small_menu = state.breakpoints[Breakpoints.XSmall];
     });
   }
 }
