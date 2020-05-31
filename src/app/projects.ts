1ade47ca2e477ceb3ff5e276c98714aed00f8e88
Mokuichi <38586357+Mokuichi147@users.noreply.github.com>
2020-05-31 17:37:11 +0900

    [add] プロジェクト一覧

---
+++ src/app/projects.ts
@@ -0,0 +1,13 @@
+export interface Project {
+  title: string;
+  year: number;
+  text: string;
+}
+
+export const Projects: Project[] = [
+  { title : 'Avater2i', year : 2019, text : '' },
+  { title : 'AR App', year : 2018, text : '' },
+  { title : 'AR Grasses', year : 2018, text : '' },
+  { title : 'SF GUI', year : 2017, text : '' },
+  { title : 'Lunchbox', year : 2017, text : '' }
+]
