d45f9c9315b80f77bfa65591b42a9e6a540a8efd
Mokuichi <38586357+Mokuichi147@users.noreply.github.com>
2020-05-31 17:37:40 +0900

    [update] プロジェクト名の表示

--- src/app/project/project.component.ts
+++ src/app/project/project.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Projects } from '../projects';
+
 @Component({
   selector: 'app-project',
   templateUrl: './project.component.html',
   styleUrls: ['./project.component.styl']
 })
 export class ProjectComponent implements OnInit {
+  projects = Projects;
+  select_index = 0;
+  project_names = [];
 
   constructor() { }
 
   ngOnInit(): void {
+    let count = 0;
+    for (let project of this.projects) {
+      this.project_names.push([]);
+      for (let text of project.title) {
+        this.project_names[count].push(text);
+      }
+      count += 1;
+    }
   }
 
 }
