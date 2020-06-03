903a768af59cba89e03f9ef8d84829c5bb9ae44d
Mokuichi <38586357+Mokuichi147@users.noreply.github.com>
2020-06-04 03:58:00 +0900

    [fix] 呉字

--- src/app/projects.ts
+++ src/app/projects.ts
@@ -5,7 +5,7 @@ export interface Project {
 }
 
 export const Projects: Project[] = [
-  { title : 'Avater2i', year : 2019, text : '' },
+  { title : 'Avatar2i', year : 2019, text : '' },
   { title : 'AR App', year : 2018, text : '' },
   { title : 'AR Grasses', year : 2018, text : '' },
   { title : 'SF GUI', year : 2017, text : '' },
