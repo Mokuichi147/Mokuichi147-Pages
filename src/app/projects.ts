dcec158988e4860f67b4759796e27f11e5f4ba56
Mokuichi <38586357+Mokuichi147@users.noreply.github.com>
2020-06-07 03:50:10 +0900

    [update] コードを見やすくした

--- src/app/projects.ts
+++ src/app/projects.ts
@@ -6,9 +6,34 @@ export interface Project {
 }
 
 export const Projects: Project[] = [
-  { title : 'Avatar2i', year : 2019, path : 'assets/images/avatar2i.jpg', text : '' },
-  { title : 'AR App', year : 2018, path : 'assets/images/ar_app.jpg', text : '' },
-  { title : 'AR Grasses', year : 2018, path : 'assets/images/ar_grasses.jpg', text : '' },
-  { title : 'SF GUI', year : 2017, path : 'assets/images/sf_gui.jpg', text : '' },
-  { title : 'Lunchbox', year : 2017, path : 'assets/images/lunchbox.jpg', text : '' }
+  {
+    title: 'Avatar2i',
+    year: 2019,
+    path: 'assets/images/avatar2i.jpg',
+    text: ''
+  },
+  {
+    title: 'AR App',
+    year: 2018,
+    path: 'assets/images/ar_app.jpg',
+    text: ''
+  },
+  {
+    title: 'AR Grasses',
+    year: 2018,
+    path: 'assets/images/ar_grasses.jpg',
+    text: ''
+  },
+  {
+    title: 'SF GUI',
+    year: 2017,
+    path: 'assets/images/sf_gui.jpg',
+    text: ''
+  },
+  {
+    title: 'Lunchbox',
+    year: 2017,
+    path: 'assets/images/lunchbox.jpg',
+    text: ''
+  }
 ]
