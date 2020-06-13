cde60267b80e60c1e05bca9c0a39b171b38ca54b
Mokuichi <38586357+Mokuichi147@users.noreply.github.com>
2020-06-13 21:30:24 +0900

    [update] toolの表示方法にmat-chipを使うようにした

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatChipsModule } from '@angular/material/chips';
 import { LayoutModule } from '@angular/cdk/layout';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ProjectComponent } from './project/project.component';
 import { AboutComponent } from './about/about.component';
@@ -26,6 +28,7 @@ import { AboutComponent } from './about/about.component';
     MatMenuModule,
     MatIconModule,
     MatButtonModule,
+    MatChipsModule,
     LayoutModule
   ],
   providers: [],
