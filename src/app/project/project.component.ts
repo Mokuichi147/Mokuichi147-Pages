866a4fca3c3d55be04d3b3fdd9d5c60fad7b6df3
Mokuichi147 <38586357+Mokuichi147@users.noreply.github.com>
2020-07-18 21:51:16 +0900

    [add] project一覧

--- src/app/project/project.component.ts
+++ src/app/project/project.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Projects } from '../projects';
+
 @Component({
   selector: 'app-project',
   templateUrl: './project.component.html',
@@ -7,6 +9,7 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class ProjectComponent implements OnInit {
+  projects = Projects;
 
   constructor() {}
 
