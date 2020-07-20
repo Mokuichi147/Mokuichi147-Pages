4da55167318bc7bfcf3d6a80b5a5970a18044960
ktpi2000 <katapi2000@gmail.com>
2020-07-20 17:24:56 +0900

    画像切替メソッドの修正

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,18 +5,20 @@ import { Projects } from '../projects';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.styl']
+  styleUrls: ['./home.component.styl'],
 })
-
 export class HomeComponent implements OnInit {
   projects = Projects;
-  select_index = 0;
+  select_index: number = 0;
   project_names = [];
   project_name_show = true;
+  imagePath: string;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.changeImage(this.select_index);
+
     // ['project_name'] -> ['p', 'r', 'o', ...]
     let count = 0;
     for (let project of this.projects) {
@@ -28,33 +30,24 @@ export class HomeComponent implements OnInit {
     }
   }
 
-  nextClick() {
-    this.project_name_show = !this.project_name_show;
-    this.select_index++;
-    console.log(this.select_index);
+  changeImage(id: number) {
+    console.log(id);
+    this.select_index += id;
     if (this.select_index >= this.projects.length) {
       this.select_index = 0;
-    }
-
-    var target_img = <HTMLImageElement>document.getElementById('main-img');
-  }
-
-  prevClick() {
-    this.project_name_show = !this.project_name_show;
-    this.select_index--;
-    console.log(this.select_index);
-    if (this.select_index < 0) {
+    } else if (this.select_index < 0) {
       this.select_index = this.projects.length - 1;
     }
 
-    var target_img = <HTMLImageElement>document.getElementById('main-img');
+    console.log(this.projects[this.select_index].path);
+    this.imagePath = this.projects[this.select_index].path;
   }
 
   moreClick() {
     console.log(this.projects[this.select_index].year);
   }
 
-  trackByIdentity (index: number, item: any) {
+  trackByIdentity(index: number, item: any) {
     // DOMを再生成するためにidではなく適当な値を返す
     return Math.random();
   }
