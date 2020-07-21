467cb61c3a99e838e0378c8e1414f9ca9f6a45bc
Mokuichi147 <38586357+Mokuichi147@users.noreply.github.com>
2020-07-22 04:47:35 +0900

    [add] プロジェクト一覧の背景画像

--- src/app/project/project.component.ts
+++ src/app/project/project.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Projects } from '../projects';
+import { UrlHandlingStrategy } from '@angular/router';
 
 @Component({
   selector: 'app-project',
@@ -10,10 +11,16 @@ import { Projects } from '../projects';
 
 export class ProjectComponent implements OnInit {
   projects = Projects;
+  project_bg_image: string;
+  show_more = false;
+  more: string;
 
   constructor() {}
 
   ngOnInit(): void {
   }
 
+  backgroundImage(path: string) {
+    this.project_bg_image = 'linear-gradient(rgba(0,0,0,0.9), rgba(0,0,0,0.9)), url(' + path + ')';
+  }
 }
